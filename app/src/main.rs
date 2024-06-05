5f79b31f1c3d2f422e2063a2a3ee79dcee45ddc4
glmcz <mates.glm@gmail.com>
2024-06-05 15:33:43 +0200

    Adding HardFaultTrampoline with asm code for user defined HardFault handler

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,7 +4,7 @@
 
 use rt::entry;
 
-use core::intrinsics;
+//use core::intrinsics;
 
 entry!(main);
 
@@ -14,14 +14,20 @@ static mut BSS: u8 = 0;
 static mut DATA: u8 = 1;
 
 fn main() -> ! {
-    intrinsics::abort();
+    //intrinsics::abort();
     // let _x = RODATA;
     // let _y = unsafe { BSS };
     // let _z = unsafe { DATA };
-    // loop{}
+    loop{}
 }
 
+#[allow(non_snake_case)]
 #[no_mangle]
-pub extern "C" fn HardFault() -> ! {
+pub fn HardFault(_ef: *const u32) -> ! {
     loop {}
 }
+
+// #[no_mangle]
+// pub extern "C" fn HardFault() -> ! {
+//     loop {}
+// }
--- rt/build.rs
+++ rt/build.rs
@@ -1,4 +1,10 @@
-use std::{env, error::Error, fs::File, io::Write, path::PathBuf};
+use std::{
+    env,
+    error::Error,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
 
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -10,6 +16,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rustc-link-search={}", out.display());
     println!("cargo:rerun-if-canged=build.rs");
     println!("cargo:rerun-if-changed=memory.x");
+
+    // link to `librt.a`
+    fs::copy("librt.a", out.join("librt.a"))?; // <- NEW!
+    println!("cargo:rustc-link-lib=static=rt");
+
+    // rebuild if `librt.a` changed
+    println!("cargo:rerun-if-changed=librt.a");
+
 
     Ok(())
 }
